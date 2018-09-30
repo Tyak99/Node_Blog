98621b6ef83ab1a0827ff82bd1f6975918250243
Yakub Babatunde <nasri@Yakubs-MacBook-Pro.local>
2018-09-30 17:39:51 +0100

    initial commit

---
+++ models/blog.js
@@ -0,0 +1,12 @@
+//mMONGOOSE MODEL/CONFIG 
+var mongoose = require("mongoose");
+
+
+var blogSchema = mongoose.Schema({
+	title: String,
+	image: String,
+	body: String,
+	created: {type: Date, default: Date.now}
+});
+
+module.exports = mongoose.model("Blog", blogSchema);
